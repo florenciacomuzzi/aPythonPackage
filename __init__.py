e6fb4a7a333aa3bc36814c9ec26f1935b63dc0c1
Flo Sal <florenciabsalinas@gmail.com>
2016-03-30 21:28:19 -0400

    add demo package

---
+++ __init__.py
@@ -0,0 +1,5 @@
+"""this makes demo dir a package"""
+
+print('demo/__init__.py is being executed')
+def main(): 
+    print('main was called')
---
+++ __main__.py
@@ -0,0 +1,5 @@
+"""this module is run when the package is the first insertion point for the program"""
+
+print('__main__.py is being executed')
+from __init__ import main
+main()
